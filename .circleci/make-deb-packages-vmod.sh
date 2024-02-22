bfa131b14375d987f7f7648697974995fe1dad27
Guillaume Quintard <guillaume.quintard@varnish-software.com>
2024-02-22 01:05:56 +0100

    auto download orig url

--- .circleci/make-deb-packages-vmod.sh
+++ .circleci/make-deb-packages-vmod.sh
@@ -30,6 +30,7 @@ apt-get update
 apt-get install -y dpkg-dev debhelper devscripts equivs pkg-config apt-utils fakeroot /deps/$PARAM_DIST/$PARAM_RELEASE/*.deb
 VVERSION="$(dpkg -l | awk '$2 == "varnish" {print $3}')"
 sed -i "s/@VVERSION@/$VVERSION/" debian/*
+curl -L "$(cat debian/orig_url)" | tar xzv --strip 1
 
 # Ubuntu 20.04 aarch64 fails when using fakeroot-sysv with:
 #    semop(1): encountered an error: Function not implemented
