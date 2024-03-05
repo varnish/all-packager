fad800173aa448abc443258c1466ed657fb4c562
henrychoiATvarnish <hchoi@varnish-software.com>
2024-03-05 09:37:56 +0800

    Update deb build script

--- .circleci/make-deb-packages-vmod.sh
+++ .circleci/make-deb-packages-vmod.sh
@@ -27,19 +27,24 @@ elif [ -z "$PARAM_DIST" ]; then
 fi
 
 apt-get update
-apt-get install -y dpkg-dev debhelper devscripts equivs pkg-config apt-utils fakeroot docutils-common python3-docutils python3-roman sgml-base xml-core /deps/$PARAM_DIST/$PARAM_RELEASE/*.deb
+apt-get install -y dpkg-dev debhelper devscripts equivs pkg-config apt-utils fakeroot /deps/$PARAM_DIST/$PARAM_RELEASE/*.deb
 VVERSION="$(dpkg -l | awk '$2 == "varnish" {print $3}' | sed 's/-.*//' )"
 sed -i "s/@VVERSION@/$VVERSION/" debian/*
-curl -L "$(cat debian/orig_url)" -o ../$(cat debian/orig_packagename) 
-
-tar xvfz ../$(cat debian/orig_packagename) --strip 1
 
 # Ubuntu 20.04 aarch64 fails when using fakeroot-sysv with:
 #    semop(1): encountered an error: Function not implemented
 update-alternatives --set fakeroot /usr/bin/fakeroot-tcp
 
-#echo "Install Build-Depends packages..."
-#yes | mk-build-deps --install debian/control || true
+echo "Install Build-Depends packages..."
+yes | mk-build-deps --install debian/control || true
+
+mkdir -p ./pkgbuild/distdir/
+cp -r debian pkgbuild/distdir/
+
+curl -L "$(cat debian/orig_url)" -o ./pkgbuild/$(cat debian/orig_packagename) 
+
+cd ./pkgbuild/distdir/
+tar xvfz ../$(cat debian/orig_packagename) --strip 1
 
 echo "Build the packages..."
 dpkg-buildpackage -us -uc -j16
