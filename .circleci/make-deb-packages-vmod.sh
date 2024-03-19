42098c5b6c9e8b2928288a51a9ec41429b37dd4b
Guillaume Quintard <guillaume.quintard@varnish-software.com>
2024-03-19 12:04:11 -0700

    revision annoyance

--- .circleci/make-deb-packages-vmod.sh
+++ .circleci/make-deb-packages-vmod.sh
@@ -18,9 +18,9 @@ apt-get install -y python*-docutils
 
 VVERSION="$(dpkg -l | awk '$2 == "varnish" {print $3}')"
 PVERSION=$(echo ${VVERSION%~*} | sed 's/[~-].*//').0
-DEB_ORIG=$(grep Source debian/control | awk -F' ' '{print $2}')_${PVERSION}-1.orig.tar.gz
+DEB_ORIG=$(grep Source debian/control | awk -F' ' '{print $2}')_${PVERSION}.orig.tar.gz
 
-sed -i -e "s/@VVERSION@/$VVERSION/" -e "s/@PVERSION@/$PVERSION/" debian/*
+sed -i -e "s/@VVERSION@/$VVERSION/" -e "s/@PVERSION@/$PVERSION-1/" debian/*
 
 cat debian/changelog
 
